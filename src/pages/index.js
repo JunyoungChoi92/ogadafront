145cdb9499b0785dc251f903633aba2897223264
jy.choi <jy.choi@zipida.com>
2023-03-30 12:02:59 +0900

    Frontend

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
                     justifyContent: "center",
                 }}
             >
-                <Weather />
+                {/* <Weather /> */}
                 <BoxGroup />
             </div>
         </>
